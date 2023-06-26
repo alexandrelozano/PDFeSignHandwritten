d5be0bd124ddab544b5b2d5e0bf7e7db0e858f20
Alexandre Lozano Vilanova <alexandre.lozano@gmail.com>
2023-06-26 09:14:00 +0200

    New release

--- PDFeSignHandwritten/Properties/AssemblyInfo.cs
+++ PDFeSignHandwritten/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PDFeSignHandwritten")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.4.0")]
+[assembly: AssemblyVersion("1.5.5.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
